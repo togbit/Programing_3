f3a9ae2e3d0d802de672908944eb98f661bf1671
Your NameDavidKarapetyan <you@example.com>
2018-12-01 15:34:58 +0400

    Arancnacnel classner

---
+++ public/Grass.js
@@ -0,0 +1,55 @@
+class Grass {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.multiply = 0;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+    chooseCell(num) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell && this.multiply >= 6) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[newY][newX] = new Grass(newX, newY, 1);
+            this.multiply = 0;
+
+        }
+        
+    }
+
+
+
+}
+
+
+
+
+
---
+++ public/GrassE.js
@@ -0,0 +1,100 @@
+class GrassEater {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.index = index;
+        this.directions = [];
+        this.acted = false;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[newY][newX] = new GrassEater(newX, newY, 2);
+        }
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+    }
+    move() {
+        var newCell = random(this.chooseCell(0));
+        if (this.acted == false) {
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+                this.x = newX;
+                this.y = newY;
+                this.acted = true;
+
+            }
+            this.energy--;
+            if (this.energy <= 0) {
+                this.die();
+            }
+
+        }
+        console.log(this.energy);
+    }
+    eat() {
+        var newCell = random(this.chooseCell(1));
+        if (this.acted == false) {
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+                this.x = newX;
+                this.y = newY;
+
+                this.acted = true;
+                this.energy++;
+                if (this.energy >= 16) {
+                    this.mul();
+                    this.energy = 10;
+                }
+            }
+            else {
+                this.move();
+            }
+        }
+    }
+}
